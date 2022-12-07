9c31c555f2fc6dcd5695ea5e80751e1908bd9f73
OtabekToshtemirov <47772571+Otabek04@users.noreply.github.com>
2022-12-07 12:44:57 +0500

    t

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+# create function Guessnumber
+def Guessnumber():
+    import random
 
-# input year
-# output function  Agecalculation
-def Agecalculation(year):
-    return 2019 - year
-print(Agecalculation(1980)
-)
+    tries = 0
+    number = random.randint(1, 100)
+    guess = int(input("Guess a number between 1 and 100: "))
+    while guess != number:
+        if guess < number:
+            print("Too low")
+            guess = int(input("Guess again: "))
+        elif guess > number:
+            print("Too high")
+            guess = int(input("Guess again: "))
+        1
+        tries += 1
+    if guess == number:
+        print("You guessed it!"+ " It took you " + str(tries+1) + " tries.")
+        print("The number was " + str(number))
 
-
-
+Guessnumber()
